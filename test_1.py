dcd139921ecff9d08ad85fd7e04ac20e5cc99636
jimmyue <jimmy_job@126.com>
2019-10-12 14:04:40 +0800

    fist commit

---
+++ test_1.py
@@ -0,0 +1,73 @@
+import pytest
+
+def test_success():
+    """this test succeeds"""
+    assert True
+
+
+def test_failure():
+    """this test fails"""
+    assert False
+
+
+def test_skip():
+    """this test is skipped"""
+    pytest.skip('for a reason!')
+
+
+def test_broken():
+    raise Exception('oops')
+
+
+@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
+def test_xfail_expected_failure():
+    """this test is an xfail that will be marked as expected failure"""
+    assert False
+
+
+@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
+def test_xfail_unexpected_pass():
+    """this test is an xfail that will be marked as unexpected success"""
+    assert True
+
+
+@pytest.fixture(params=[True, False], ids=['param_true', 'param_false'])
+def function_scope_fixture_with_finalizer(request):
+    if request.param:
+        print('True')
+    else:
+        print('False')
+    def function_scope_finalizer():
+        function_scope_step()
+    request.addfinalizer(function_scope_finalizer)
+
+
+@pytest.fixture(scope='class')
+def class_scope_fixture_with_finalizer(request):
+    def class_finalizer_fixture():
+        class_scope_step()
+    request.addfinalizer(class_finalizer_fixture)
+
+
+@pytest.fixture(scope='module')
+def module_scope_fixture_with_finalizer(request):
+    def module_finalizer_fixture():
+        module_scope_step()
+    request.addfinalizer(module_finalizer_fixture)
+
+
+@pytest.fixture(scope='session')
+def session_scope_fixture_with_finalizer(request):
+    def session_finalizer_fixture():
+        session_scope_step()
+    request.addfinalizer(session_finalizer_fixture)
+
+
+class TestClass(object):
+
+    def test_with_scoped_finalizers(self,
+                                    function_scope_fixture_with_finalizer,
+                                    class_scope_fixture_with_finalizer,
+                                    module_scope_fixture_with_finalizer,
+                                    session_scope_fixture_with_finalizer):
+        step_inside_test_body()
